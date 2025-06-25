627ce73bb685b7dc0e9b1b8dd602b324f8b67887
Riley Ness <rileyness@MacBookPro.lan>
2025-06-24 21:58:04 -0500

    Initial commit

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheoryAndPixel.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
