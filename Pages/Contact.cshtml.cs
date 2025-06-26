9f2edf21e649f1b9e918f4ca1470a8bf9d42873f
Riley Ness <rileyness@MacBookPro.lan>
2025-06-25 23:00:36 -0500

    Updated contact form and layout

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -1,12 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace TheoryAndPixel.Pages
 {
-    public class ContactModel : PageModel
-    {
-        public void OnGet()
-        {
-            
-        }
-    }
+public class ContactModel : PageModel
+{
+[BindProperty]
+public ContactForm Contact { get; set; } = new ContactForm();
+
+public void OnGet() { }
+
+public IActionResult OnPost()
+{
+if (!ModelState.IsValid)
+return Page();
+
+// You can handle the form submission here.
+return RedirectToPage("ThankYou"); // or stay on the same page
+}
+}
+
+public class ContactForm
+{
+[Required]
+public string Name { get; set; }
+
+[Required, EmailAddress]
+public string Email { get; set; }
+
+[Required]
+public string Message { get; set; }
+}
 }
